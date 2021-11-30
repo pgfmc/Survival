d0218101371d7d680f95e0a08a0be099cbf58ae5
bk <33919755+bkiscool@users.noreply.github.com>
2021-11-30 09:21:30 +0000

    Add files via upload

---
+++ src/net/pgfmc/survival/Main.java
@@ -0,0 +1,21 @@
+package net.pgfmc.survival;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class Main extends JavaPlugin {
+	
+	@Override
+	public void onEnable()
+	{
+		
+	}
+	
+	@Override
+	public void onDisable()
+	{
+		
+	}
+	
+	
+
+}
